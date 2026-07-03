d9905e6c5458ce2aebdbf7a66fe62a66df2d66e7
Ayyayush <treddytalks@gmail.com>
2026-07-03 17:27:47 +0530

    3-Production-Backend

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,83 +1,70 @@
 const { Server } = require("socket.io");
-const Chat = require("../models/Chat");
+const Chat = require("../models/chat");
 
 const initializeSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      credentials: true,
+    },
+  });
 
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:5173",
-            credentials: true,
-        },
+  io.on("connection", (socket) => {
+    console.log("User Connected :", socket.id);
+
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const room = [userId, targetUserId].sort().join("_");
+
+      socket.join(room);
+
+      console.log(`${firstName} joined room ${room}`);
     });
 
-    io.on("connection", (socket) => {
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, targetUserId, text }) => {
+        try {
+          const room = [userId, targetUserId].sort().join("_");
 
-        console.log("User Connected :", socket.id);
+          let chat = await Chat.findOne({
+            participants: {
+              $all: [userId, targetUserId],
+            },
+          });
 
-        socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+          if (!chat) {
+            chat = await Chat.create({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
 
-            const room = [userId, targetUserId].sort().join("_");
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
 
-            socket.join(room);
+          await chat.save();
 
-            console.log(`${firstName} joined room ${room}`);
+          // Populate latest message
+          chat = await Chat.findById(chat._id).populate(
+            "messages.senderId",
+            "firstName lastName photoUrl"
+          );
 
-        });
+          const latestMessage = chat.messages[chat.messages.length - 1];
 
-        socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
+          io.to(room).emit("messageReceived", latestMessage);
+        } catch (err) {
+          console.error("Socket Error:", err);
+        }
+      }
+    );
 
-            try {
-
-                const room = [userId, targetUserId].sort().join("_");
-
-                let chat = await Chat.findOne({
-                    participants: {
-                        $all: [userId, targetUserId],
-                    },
-                });
-
-                if (!chat) {
-
-                    chat = await Chat.create({
-                        participants: [userId, targetUserId],
-                        messages: [],
-                    });
-
-                }
-
-                chat.messages.push({
-                    senderId: userId,
-                    text,
-                });
-
-                await chat.save();
-
-                // Populate only the latest message
-                chat = await Chat.findById(chat._id).populate(
-                    "messages.senderId",
-                    "firstName lastName photoUrl"
-                );
-
-                const latestMessage = chat.messages[chat.messages.length - 1];
-
-                io.to(room).emit("messageReceived", latestMessage);
-
-            } catch (err) {
-
-                console.log(err);
-
-            }
-
-        });
-
-        socket.on("disconnect", () => {
-
-            console.log("User Disconnected :", socket.id);
-
-        });
-
+    socket.on("disconnect", () => {
+      console.log("User Disconnected :", socket.id);
     });
-
+  });
 };
 
 module.exports = initializeSocket;

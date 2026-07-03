e084fcd683b5af4361c2fd51d9155776ec1832c4
Ayyayush <treddytalks@gmail.com>
2026-07-03 17:34:47 +0530

    4-Production-Backend

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const profileRouter = express.Router();
 
-const User = require("../models/User");
+const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
 

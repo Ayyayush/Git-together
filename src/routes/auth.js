5d07e301e8f6d81f9b8b4a426a1386fea52b0bf9
Ayyayush <treddytalks@gmail.com>
2026-07-03 21:09:34 +0530

    7-Production-Backend

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -60,7 +60,8 @@ authRouter.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
